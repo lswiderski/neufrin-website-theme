2257368318a8ce98bb22e8e8590e10059bae0c31
Neufrin <mail@lukaszswiderski.com>
2015-07-13 21:57:18 +0200

    posts loop

--- index.php
+++ index.php
@@ -19,83 +19,32 @@
         <!-- Main Content -->
         <div class="container">
             <div class="row">
-            <?php if ( have_posts() ) : ?>
-    <?php while ( have_posts() ) : the_post(); ?>
-      <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <div class="post-header">
-            <div class="date"><?php the_time( 'M j y' ); ?></div>
-            <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-            <div class="author"><?php the_author(); ?></div>
-        </div><!--end post header-->
-        <div class="entry clear">
-            <?php if ( function_exists( 'add_theme_support' ) ) the_post_thumbnail(); ?>
-            <?php the_content(); ?>
-            <?php edit_post_link(); ?>
-            <?php wp_link_pages(); ?>
-        </div><!--end entry-->
-        <div class="post-footer">
-            <div class="comments"><?php comments_popup_link( 'Leave a Comment', '1 Comment', '% Comments' ); ?></div>
-        </div><!--end post footer-->
-      </div><!--end post-->
-    <?php endwhile; /* rewind or continue if all posts have been fetched */ ?>
-      <div class="navigation index">
-        <div class="alignleft"><?php next_posts_link( 'Older Entries' ); ?></div>
-        <div class="alignright"><?php previous_posts_link( 'Newer Entries' ); ?></div>
-      </div><!--end navigation-->
-    <?php else : ?>
-    <?php endif; ?>
+                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
 
+
+<?php /* The loop */ ?>
+            <?php while ( have_posts() ) : the_post(); ?>
+
+                
+                    <div class="post-preview  <?php post_class(); ?>" id="post-<?php the_ID(); ?> ">
+                        <a href="<?php the_permalink(); ?>" rel="bookmark"  title="Permanent Link to <?php the_title_attribute(); ?>">
+                            <h2 class="post-title">
+                               <?php the_title(); ?>
+                            </h2>
+                            <h3 class="post-subtitle">
+                               <?php echo get_the_excerpt(); ?>
+                            </h3>
+                        </a>
+                        <p class="post-meta">Posted by <a href="#"><?php the_author_posts_link(); ?></a> on <?php the_time('F jS, Y'); ?></p>
+                    </div>
+                    <hr>
+            <?php endwhile; ?>
     
-                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                    <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">
-                                Man must explore, and this is exploration at its greatest
-                            </h2>
-                            <h3 class="post-subtitle">
-                                Problems look mighty small from 150 miles up
-                            </h3>
-                        </a>
-                        <p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on September 24, 2014</p>
-                    </div>
-                    <hr>
-                    <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">
-                                I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
-                            </h2>
-                        </a>
-                        <p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on September 18, 2014</p>
-                    </div>
-                    <hr>
-                    <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">
-                                Science has not yet mastered prophecy
-                            </h2>
-                            <h3 class="post-subtitle">
-                                We predict too much for the next year and yet far too little for the next ten.
-                            </h3>
-                        </a>
-                        <p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on August 24, 2014</p>
-                    </div>
-                    <hr>
-                    <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">
-                                Failure is not an option
-                            </h2>
-                            <h3 class="post-subtitle">
-                                Many say exploration is part of our destiny, but it’s actually our duty to future generations.
-                            </h3>
-                        </a>
-                        <p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on July 8, 2014</p>
-                    </div>
-                    <hr>
+            
                     <!-- Pager -->
                     <ul class="pager">
                         <li class="next">
-                            <a href="#">Older Posts &rarr;</a>
+                        <?php next_posts_link('Older Posts &rarr;'); ?>
                         </li>
                     </ul>
                 </div>
---
+++ single.php
@@ -0,0 +1,64 @@
+<?php
+/**
+*S ingle Post Template: Post
+*/
+?>
+
+
+<?php get_header(); ?>
+
+    <!-- Page Header -->
+    <!-- Set your background image for this header on the line below. -->
+    <header class="intro-header" style="background-image: url('img/post-bg.jpg')">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <div class="post-heading">
+                        <h1>Man must explore, and this is exploration at its greatest</h1>
+                        <h2 class="subheading">Problems look mighty small from 150 miles up</h2>
+                        <span class="meta">Posted by <a href="#">Start Bootstrap</a> on August 24, 2014</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Post Content -->
+    <article>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+             <?php /* The loop */ ?>
+            <?php while ( have_posts() ) : the_post(); ?>
+
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <header class="entry-header">
+                        <?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
+                        <div class="entry-thumbnail">
+                            <?php the_post_thumbnail(); ?>
+                        </div>
+                        <?php endif; ?>
+
+                        <h1 class="entry-title"><?php the_title(); ?></h1>
+                    </header><!-- .entry-header -->
+
+                    <div class="entry-content">
+                        <?php the_content(); ?>
+                        <?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
+                    </div><!-- .entry-content -->
+
+                    <footer class="entry-meta">
+                        <?php edit_post_link( __( 'Edit', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
+                    </footer><!-- .entry-meta -->
+                </article><!-- #post -->
+
+                <?php comments_template(); ?>
+            <?php endwhile; ?>
+                </div>
+            </div>
+        </div>
+    </article>
+
+ 
+ <?php get_sidebar(); ?>
+<?php get_footer(); ?>
