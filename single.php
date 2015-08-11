eb5ca4c6ef4a861e5791328df2b01c03165d85db
Lukasz Swiderski <mail@lukaszswiderski.com>
2015-08-11 21:52:54 +0200

    Back Button

--- single.php
+++ single.php
@@ -18,6 +18,12 @@
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                         <?php the_content(); ?>
             <?php endwhile; ?>
+            <ul class="pager">
+                    <li class="next">
+                        <a href="javascript:history.go(-1)" onMouseOver="self.status=document.referrer;return true">Back</a>
+                    </li>
+                </ul>
+            
                 </div>
             </div>
         </div>
